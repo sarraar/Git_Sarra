66955368374862eebf01494c25294a0946099e3e
sarraar <sarraatay@gmail.com>
2022-03-17 19:02:41 +0100

    AA

---
+++ src/test/java/day1/Hellogit.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Hellogit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("2. satir calismaz");
+    }
+}
